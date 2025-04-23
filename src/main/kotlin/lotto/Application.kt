d0092da64e678827f68597f41dcef5f2632c47dd
Irene Rivero <irene.rivero.casal@gmail.com>
2025-04-23 14:24:56 +0200

    feat(main): implement basic ticket purchase functionality

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
 
+import lotto.view.InputView
+import lotto.view.OutputView
+import lotto.domain.LottoGenerator
+
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val outputView = OutputView()
+    val lottoGenerator = LottoGenerator()
+    try {
+        val purchaseAmount = inputView.readPurchaseAmount()
+        val lottoCount = purchaseAmount / 1000
+        val lottos = lottoGenerator.generate(lottoCount)
+        outputView.printPurchasedLottos(lottos)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
